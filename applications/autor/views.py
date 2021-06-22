cef2a55afc51b1f398cde40c84047e058d161e69
Carlos Martinez <carlosmartiguerre@gmail.com>
2021-06-22 13:15:56 +0200

    Crear view para autor y preparacion del manager

--- applications/autor/views.py
+++ applications/autor/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+
+# Local models
+from .models import Autor
+
 # Create your views here.
+class ListAutores(ListView):
+    context_object_name = 'lista_autores'
+    template_name = 'autor/lista.html'
+
+    def get_queryset(self):
+        return Autor.objects.all()
