e826dcfbb042e5bf0b14cb88bee37c1e92a19524
Carlos Martinez <carlosmartiguerre@gmail.com>
2021-06-24 12:19:07 +0200

    Filtros fechas en managers en Libro

--- applications/libro/managers.py
+++ applications/libro/managers.py
@@ -1,7 +1,25 @@
 from django.db import models
 
+# datetime para validar fechas recibidas por el html
+import datetime
+
 class LibroManager(models.Manager):
     ''' managers para el modelo libro '''
 
-    def listar_libros(self):
-        return self.all()
+    def listar_libros(self, kword):
+        result = self.filter(
+            titulo__icontains=kword,
+            fecha__range=('2020-06-01', '2022-01-01')
+        )
+        return result
+
+    def listar_libros_date(self, kword, fecha1, fecha2):
+
+        date1 = datetime.datetime.strptime(fecha1, '%Y-%m-%d').date() # Mayus para valor de 4 cifras / minus para 2 cifras
+        date2 = datetime.datetime.strptime(fecha2, '%Y-%m-%d').date()
+
+        result = self.filter(
+            titulo__icontains=kword,
+            fecha__range=(date1, date2)
+        )
+        return result
--- applications/libro/views.py
+++ applications/libro/views.py
@@ -11,4 +11,13 @@ class ListLibros(ListView):
     context_object_name = 'lista_libros'
 
     def get_queryset(self):
-        return Libro.objects.listar_libros()
+        clave = self.request.GET.get('kword','')
+        f1 = self.request.GET.get('fecha1','')
+        f2 = self.request.GET.get('fecha2','')
+
+        if f1 and f2:
+            return Libro.objects.listar_libros_date(clave,f1,f2)
+        else:
+            return Libro.objects.listar_libros(clave)
+
+        
